2911516242e94063864e3141564343f4bd75ffcb
aac190002 <66142794+aac190002@users.noreply.github.com>
2020-07-02 17:37:06 -0500

    Followed along with class videos for smartsaveui.py

--- src/smartsaveui.py
+++ src/smartsaveui.py
@@ -5,3 +5,132 @@ Assignment 02
 ATCM 3311.0U1
 07/03/2020
 """
+
+import maya.OpenMayaUI as omui
+from PySide2 import QtWidgets, QtCore
+from shiboken2 import wrapInstance
+
+import mayautils
+
+
+def maya_main_window():
+    """Return the maya main window widget"""
+    main_window = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window), QtWidgets.QWidget)
+
+
+class SmartSaveUI(QtWidgets.QDialog):
+    """Smart Save UI Class"""
+
+    def __init__(self):
+        """Constructor"""
+        # Passing SmartSaveUI makes this Python 2 and 3 compatible
+        super(SmartSaveUI, self).__init__(parent=maya_main_window())
+        self.scene = mayautils.SceneFile(current_scene=True)
+        self.setWindowTitle("Smart Save")
+        self.resize(500, 200)
+        self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+        self.create_widgets()
+        self.create_layout()
+        self.create_connections()
+
+    def create_widgets(self):
+        """Create widgets for our UI"""
+        self.title_lbl = QtWidgets.QLabel("Smart Save")
+        self.title_lbl.setStyleSheet("font: bold 20px")
+
+        # Directory
+        self.dir_lbl = QtWidgets.QLabel("Directory")
+        self.dir_le = QtWidgets.QLineEdit()
+        self.dir_le.setText(self.scene.dir)
+        self.browse_btn = QtWidgets.QPushButton("Browse...")
+
+        # Descriptor
+        self.descriptor_lbl = QtWidgets.QLabel("Descriptor")
+        self.descriptor_le = QtWidgets.QLineEdit()
+        self.descriptor_le.setText(self.scene.descriptor)
+
+        # Version
+        self.version_lbl = QtWidgets.QLabel("Version")
+        self.version_spinbox = QtWidgets.QSpinBox()
+        self.version_spinbox.setValue(self.scene.version)
+
+        # Extension
+        self.ext_lbl = QtWidgets.QLabel("Extension")
+        self.ext_le = QtWidgets.QLineEdit()
+        self.ext_le.setText(self.scene.ext)
+
+        # Buttons
+        self.save_btn = QtWidgets.QPushButton("Save")
+        self.increment_save_btn = QtWidgets.QPushButton("Increment and Save")
+        self.cancel_btn = QtWidgets.QPushButton("Cancel")
+
+    def create_layout(self):
+        """Lay out our widgets in the UI"""
+        # Directory
+        self.directory_lay = QtWidgets.QHBoxLayout()
+        self.directory_lay.addWidget(self.dir_lbl)
+        self.directory_lay.addWidget(self.dir_le)
+        self.directory_lay.addWidget(self.browse_btn)
+
+        # Descriptor
+        self.descriptor_lay = QtWidgets.QHBoxLayout()
+        self.descriptor_lay.addWidget(self.descriptor_lbl)
+        self.descriptor_lay.addWidget(self.descriptor_le)
+
+        # Version
+        self.version_lay = QtWidgets.QHBoxLayout()
+        self.version_lay.addWidget(self.version_lbl)
+        self.version_lay.addWidget(self.version_spinbox)
+
+        # Extension
+        self.extension_lay = QtWidgets.QHBoxLayout()
+        self.extension_lay.addWidget(self.ext_lbl)
+        self.extension_lay.addWidget(self.ext_le)
+
+        # Buttons
+        self.bottom_btn_lay = QtWidgets.QHBoxLayout()
+        self.bottom_btn_lay.addWidget(self.save_btn)
+        self.bottom_btn_lay.addWidget(self.increment_save_btn)
+        self.bottom_btn_lay.addWidget(self.cancel_btn)
+
+        # Main
+        self.main_layout = QtWidgets.QVBoxLayout()
+        self.main_layout.addWidget(self.title_lbl)
+        self.main_layout.addLayout(self.directory_lay)
+        self.main_layout.addLayout(self.descriptor_lay)
+        self.main_layout.addLayout(self.version_lay)
+        self.main_layout.addLayout(self.extension_lay)
+        self.main_layout.addStretch()
+        self.main_layout.addLayout(self.bottom_btn_lay)
+        self.setLayout(self.main_layout)
+
+    def create_connections(self):
+        """Connect our widgets signals to slots"""
+        self.cancel_btn.clicked.connect(self.cancel)
+        self.save_btn.clicked.connect(self.save)
+        self.increment_save_btn.clicked.connect(self.increment_save)
+
+    def _populate_scene_file_properties(self):
+        """Populate the SceneFile object's properties from the UI"""
+        self.scene.dir = self.dir_le.text()
+        self.scene.descriptor = self.descriptor_le.text()
+        self.scene.version = self.version_spinbox.value()
+        self.scene.ext = self.ext_le.text()
+
+    @QtCore.Slot()
+    def increment_save(self):
+        """Automatically increments to next version and saves the scene file"""
+        self._populate_scene_file_properties()
+        self.scene.increment_and_save()
+
+    @QtCore.Slot()
+    def save(self):
+        """Saves the scene file"""
+        self._populate_scene_file_properties()
+        self.scene.save()
+
+    @QtCore.Slot()
+    def cancel(self):
+        """Quits the dialog"""
+        self.close()
